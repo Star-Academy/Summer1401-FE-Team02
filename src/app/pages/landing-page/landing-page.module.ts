a665619e375e88f0ae0244b7b1d2dd9bc5ac908e
maryamsadat <razavi.mst@gmail.com>
2022-07-20 03:46:54 +0430

    [ADD] header, footer, first-banner

--- src/app/pages/landing-page/landing-page.module.ts
+++ src/app/pages/landing-page/landing-page.module.ts
@@ -3,12 +3,15 @@ import {CommonModule} from '@angular/common';
 import {LandingPageComponent} from './landing-page.component';
 import {RouterModule} from "@angular/router";
 import {PostCardComponent} from "../../components/post-card/post-card.component";
+import {HeaderModule} from "../../components/header/header.module";
+import {FooterModule} from "../../components/footer/footer.module";
+import { FirstBannerComponent } from './components/first-banner/first-banner.component';
 
 @NgModule({
     declarations: [
-        LandingPageComponent, PostCardComponent
+        LandingPageComponent, PostCardComponent, FirstBannerComponent
     ],
-    imports: [CommonModule,RouterModule],
+    imports: [CommonModule, RouterModule, HeaderModule, FooterModule],
 })
 export class LandingPageModule {
 }
