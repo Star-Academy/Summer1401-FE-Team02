3edcdcf395b14d2009fe3b3f3e561bbac8db6f16
Negarnbkhti42 <norob35@gmail.com>
2022-07-19 15:13:43 +0430

    Set postcard text position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { PostCardComponent } from './components/post-card/post-card.component';
+import {PostCardComponent} from './components/post-card/post-card.component';
 
 @NgModule({
     declarations: [AppComponent, PostCardComponent],
--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-post-card',
-  templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.scss']
+    selector: 'app-post-card',
+    templateUrl: './post-card.component.html',
+    styleUrls: ['./post-card.component.scss'],
 })
-export class PostCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PostCardComponent {
+    @Input() public poster: string = 'assets/minecraft.jpg';
+    @Input() public title: string = 'minecraft';
+    @Input() public price: string = '9';
+    @Input() public description: string =
+        'Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe!';
+    @Input() public link: string = '#';
 }
