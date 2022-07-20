76c872c5ecbca09455e99255f10b2dbbe7f49d56
Negarnbkhti42 <norob35@gmail.com>
2022-07-20 14:39:44 +0430

    Remove comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 
-const routes: Routes = [
-    {path: '', pathMatch: 'full', component: LandingPageComponent},
-    // {path: '**', component: NotFoundComponent},
-];
+const routes: Routes = [{path: '', pathMatch: 'full', component: LandingPageComponent}];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
