9f9126cf55ec77c702d3d509603ac0abb7235496
Negarnbkhti42 <norob35@gmail.com>
2022-07-24 16:35:47 +0430

    fix numbers

--- src/app/data/Games.ts
+++ src/app/data/Games.ts
@@ -5,82 +5,82 @@ export const GAMES: Game[] = [
         title: "Wonder boy: The dragon's trap",
         description:
             'به دنبال درمانی برای طلسم خود، شما در یک ماجراجویی به دنبال ابزاری جادویی می‌گردید که میتواند طلسم شما را باطل کند.',
-        price: 10000,
+        price: 10_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/e488839155be4d05bb14f6e05129daef/download-wonder-boy-the-dragons-trap-offer-e0awf.jpg?h=854&resize=1&w=360',
     },
     {
         title: 'FlatOut',
         description: 'یک مسابقه ماشین سواری پرهیجان با گرافیک شگفت‌انگیر و دستانی نوآوورانه',
-        price: 25000,
+        price: 25_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/c5ec8913ad38441b82a36c603be63e3a/download-flatout-offer-ogxv1.png?h=854&resize=1&w=360',
     },
     {
         title: 'Mari & Bayu',
         description:
             'در این بازی معمایی و ماجراجویانه، نقش مالی مورچه و مایو شب‌پره را بازی کنید و پازل‌های بازی را حل کنید. ',
-        price: 13000,
+        price: 13_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/877ee56178af4432a4565f163b60aef7/download-mari-and-bayu--the-road-home-offer-1r5s7.jpg?h=480&resize=1&w=360',
     },
     {
         title: 'Hazel',
         description:
             'در دنیای ماینکرفت با هیولاها مبارزه کنید و مسیر خود را در دنیای الهام گرفته شده از بازی کلاسیک dungeon crawlers به سوی موفقیت بیابید.',
-        price: 9000,
+        price: 9_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/576b927905694714b508b28b5d410243/download-hazel-sky-offer-1imj1.png?h=854&resize=1&w=360',
     },
     {
         title: 'Endling',
         description:
             'در دنیای ماینکرفت با هیولاها مبارزه کنید و مسیر خود را در دنیای الهام گرفته شده از بازی کلاسیک dungeon crawlers به سوی موفقیت بیابید.',
-        price: 9000,
+        price: 9_000,
         poster: 'https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_EndlingExtinctionisForever_HerobeatStudios_S2_1200x1600-490c5a4c4391f2cdd095b204910c90b1?h=854&resize=1&w=360',
     },
     {
         title: 'Mari & Bayu',
         description:
             'در این بازی معمایی و ماجراجویانه، نقش مالی مورچه و مایو شب‌پره را بازی کنید و پازل‌های بازی را حل کنید. ',
-        price: 13000,
+        price: 13_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/877ee56178af4432a4565f163b60aef7/download-mari-and-bayu--the-road-home-offer-1r5s7.jpg?h=480&resize=1&w=360',
     },
     {
         title: "Wonder boy: The dragon's trap",
         description:
             'به دنبال درمانی برای طلسم خود، شما در یک ماجراجویی به دنبال ابزاری جادویی می‌گردید که میتواند طلسم شما را باطل کند.',
-        price: 10000,
+        price: 10_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/e488839155be4d05bb14f6e05129daef/download-wonder-boy-the-dragons-trap-offer-e0awf.jpg?h=854&resize=1&w=360',
     },
     {
         title: 'FlatOut',
         description: 'یک مسابقه ماشین سواری پرهیجان با گرافیک شگفت‌انگیر و دستانی نوآوورانه',
-        price: 25000,
+        price: 25_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/c5ec8913ad38441b82a36c603be63e3a/download-flatout-offer-ogxv1.png?h=854&resize=1&w=360',
     },
     {
         title: 'Mari & Bayu',
         description:
             'در این بازی معمایی و ماجراجویانه، نقش مالی مورچه و مایو شب‌پره را بازی کنید و پازل‌های بازی را حل کنید. ',
-        price: 13000,
+        price: 13_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/877ee56178af4432a4565f163b60aef7/download-mari-and-bayu--the-road-home-offer-1r5s7.jpg?h=480&resize=1&w=360',
     },
     {
         title: 'Hazel',
         description:
             'در دنیای ماینکرفت با هیولاها مبارزه کنید و مسیر خود را در دنیای الهام گرفته شده از بازی کلاسیک dungeon crawlers به سوی موفقیت بیابید.',
-        price: 9000,
+        price: 9_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/576b927905694714b508b28b5d410243/download-hazel-sky-offer-1imj1.png?h=854&resize=1&w=360',
     },
     {
         title: 'Endling',
         description:
             'در دنیای ماینکرفت با هیولاها مبارزه کنید و مسیر خود را در دنیای الهام گرفته شده از بازی کلاسیک dungeon crawlers به سوی موفقیت بیابید.',
-        price: 9000,
+        price: 9_000,
         poster: 'https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_EndlingExtinctionisForever_HerobeatStudios_S2_1200x1600-490c5a4c4391f2cdd095b204910c90b1?h=854&resize=1&w=360',
     },
     {
         title: 'Mari & Bayu',
         description:
             'در این بازی معمایی و ماجراجویانه، نقش مالی مورچه و مایو شب‌پره را بازی کنید و پازل‌های بازی را حل کنید. ',
-        price: 13000,
+        price: 13_000,
         poster: 'https://cdn1.epicgames.com/spt-assets/877ee56178af4432a4565f163b60aef7/download-mari-and-bayu--the-road-home-offer-1r5s7.jpg?h=480&resize=1&w=360',
     },
 ];
