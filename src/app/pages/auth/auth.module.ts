5a729fbe4dc7ba89a1a5204a8530c1168ccdad60
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 20:06:58 +0430

    Remove deleted component

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,14 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FooterModule} from 'src/app/components/footer/footer.module';
-import {HeaderModule} from 'src/app/components/header/header.module';
 import {RouterModule} from '@angular/router';
 import {AuthComponent} from './auth.component';
-import {TextFieldComponent} from 'src/app/components/text-field/text-field.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    declarations: [AuthComponent, TextFieldComponent],
+    declarations: [AuthComponent],
     imports: [CommonModule, RouterModule, FormsModule],
 })
 export class AuthModule {}
