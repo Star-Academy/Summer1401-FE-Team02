bc2c76d2cd843506811b1f60d038e36b9027b3ed
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 12:13:58 +0430

    Lint configure

---
+++ src/app/utils/api.utils.ts
@@ -0,0 +1,19 @@
+export const BASE_URL = 'https://games.bijanprogrammer.com';
+
+export const USER_LOGIN = BASE_URL + '/user/login';
+export const USER_SIGNUP = BASE_URL + '/user/register';
+export const USER_AUTHENTICATE = BASE_URL + '/user/auth';
+
+export const POST_INIT = {
+    method: 'post',
+    headers: {
+        'Content-type': 'application/json',
+    },
+};
+
+export const GET_INIT = {
+    method: 'get',
+    headers: {
+        'Content-type': 'application/json',
+    },
+};
