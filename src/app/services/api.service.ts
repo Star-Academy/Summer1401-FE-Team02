cdb49f614c6a4e7bcdff7bb131c1a4d08ada33a5
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 16:16:27 +0430

    Setup toast service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
 import {POST_INIT, GET_INIT} from '../utils/api.utils';
+import {ToastService} from './toast.service';
+import {ToastType} from './ToastType.enum';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ApiService {
+    public constructor(private toastService: ToastService) {}
+
     public async post<T>(url: string, body: any = '', init: Partial<RequestInit> = {}): Promise<T | null> {
         let response = await fetch(url, {...POST_INIT, body: JSON.stringify(body), ...init});
         let data = await response.json();
@@ -12,6 +16,8 @@ export class ApiService {
         if (response.status === 200) {
             return data as T;
         }
+
+        this.toastService.show('hi', ToastType.WARNING);
 
         return null;
     }
---
+++ src/app/services/toast.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {ToastComponent} from '../components/toast/toast.component';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ToastService {
+    private toast: ToastComponent | null = null;
+
+    public setComponent(toast: ToastComponent): void {
+        this.toast = toast;
+    }
+
+    public show(message: string, color: string): void {
+        this.toast?.show(message, color);
+    }
+}
