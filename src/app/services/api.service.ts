d7d14c5de9d9b4db7f2c261e1161f295dc1986d7
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 16:18:22 +0430

    Using toast on api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {Error} from '../interfaces/Error.interface';
 import {POST_INIT, GET_INIT} from '../utils/api.utils';
 import {ToastService} from './toast.service';
 import {ToastType} from './ToastType.enum';
@@ -17,19 +18,20 @@ export class ApiService {
             return data as T;
         }
 
-        this.toastService.show('hi', ToastType.WARNING);
+        this.toastService.show((data as Error).message, ToastType.WARNING);
 
         return null;
     }
 
     public async get<T>(url: string, init: Partial<RequestInit> = {}): Promise<T | null> {
-        let response = await fetch(url, {...POST_INIT, ...init});
+        let response = await fetch(url, {...GET_INIT, ...init});
         let data = await response.json();
 
         if (response.status === 200) {
             return data as T;
         }
 
+        this.toastService.show((data as Error).message, ToastType.WARNING);
         return null;
     }
 }
