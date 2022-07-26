d740fc365b8537bd98f0334c934167b4bed41fae
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 16:16:08 +0430

    Change toast style

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,5 +1,6 @@
 import {state, style, trigger} from '@angular/animations';
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToastService} from 'src/app/services/toast.service';
 
 @Component({
     selector: 'app-toast',
@@ -24,21 +25,36 @@ import {Component} from '@angular/core';
         ]),
     ],
 })
-export class ToastComponent {
+export class ToastComponent implements OnInit {
     private interval: number | null = null;
-    public isVisible: boolean = false;
-    public constructor() {}
 
-    public show(): void {
+    public isVisible: boolean = false;
+    public message: string = '';
+    public color: string = '';
+
+    public constructor(private service: ToastService) {}
+
+    public ngOnInit(): void {
+        this.service.setComponent(this);
+    }
+
+    public show(message: string, color: string): void {
         this.interval && clearInterval(this.interval);
+        this.interval = null;
+
+        this.message = message;
+        this.color = color;
+
         this.isVisible = true;
         this.interval = setInterval(() => {
             this.isVisible = false;
+            this.interval = null;
         }, 3000);
     }
 
     public remove(): void {
         this.interval && clearInterval(this.interval);
+        this.interval = null;
         this.isVisible = false;
     }
 }
