7c18420c89c6dc1cbdda5a6aa96bdb7ef05c325a
Negarnbkhti42 <norob35@gmail.com>
2022-07-27 10:09:19 +0430

    Add conditional attribute to password

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+    public isLogged: boolean = false;
+    public constructor(private authService: AuthService) {}
+
+    public async ngOnInit(): Promise<void> {
+        let isLogged = await this.authService.isLoggedIn();
+    }
+}
