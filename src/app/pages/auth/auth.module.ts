9b276e13412334e639d23f21382bc02024a6685b
Negarnbkhti42 <norob35@gmail.com>
2022-07-31 15:49:05 +0430

    [ADD] text-input component

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AuthComponent} from './auth.component';
 import {FormsModule} from '@angular/forms';
+import {TextInputComponent} from 'src/app/components/text-input/text-input.component';
 
 @NgModule({
-    declarations: [AuthComponent],
+    declarations: [AuthComponent, TextInputComponent],
     imports: [CommonModule, RouterModule, FormsModule],
 })
 export class AuthModule {}
