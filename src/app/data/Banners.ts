e7a52d43a9aa67c7bf7313d077b19d8a2fe5c42f
msrazavi <razavi.mst@gmail.com>
2022-08-01 04:15:24 +0430

    some fixes

--- src/app/data/Banners.ts
+++ src/app/data/Banners.ts
@@ -2,25 +2,30 @@ import {Ibanner} from 'src/app/interfaces/IBanner.interface';
 
 export const BANNERS: Ibanner[] = [
     {
+        title: 'Aery Vikings',
         imageAddress: 'assets/images/posters/aery-vikings.webp',
         description:
             'سفر خود را در یک دنیای شگفت انگیز، هیجان انگیز و نفس گیر انجام دهید و از احساس پرواز در جستجوی Valhalla لذت ببرید. برای نجات وایکینگ ها از سرنوشت خود، یک خط داستانی جالب را در یک دنیای صلح آمیز و خارق العاده دنبال کنید. خدایان نورس را متقاعد کنید که ارزش نعمت آنها را دارید و مهارت های مانور باورنکردنی خود را نشان دهید.',
     },
     {
+        title: 'Cuphead',
         imageAddress: 'assets/images/posters/cuphead.webp',
         description:
             'با سلاح‌های جدید، جذابیت‌های جدید، و توانایی‌های کاملاً جدید خانم Chalice، گروه جدیدی از رئیس‌های چند وجهی و پر از صفحه را بپذیرید تا به سرآشپز Saltbaker در آخرین تلاش چالش‌برانگیز Cuphead کمک کنند.',
     },
     {
+        title: 'Elden Ring',
         imageAddress: 'assets/images/posters/elden-ring.webp',
         description: 'درام حماسی برآمده از یک اسطوره، افکار مختلف شخصیت ها در سرزمین های بین تلاقی می کنند.',
     },
     {
+        title: 'F1 - 22',
         imageAddress: 'assets/images/posters/f1-22.webp',
         description:
             'وارد دوره جدید فرمول 1 در EA SPORTS™ F1® 22، بازی ویدیویی رسمی مسابقات قهرمانی جهانی فرمول یک FIA 2022 شوید.',
     },
     {
+        title: 'Humankind',
         imageAddress: 'assets/images/posters/humankind.webp',
         description:
             'تمدن خود را ایجاد کنید. تا 60 فرهنگ تاریخی را با هم ترکیب کنید تا مردم خود را از دوران باستان به عصر مدرن هدایت کنید. از ریشه های فروتن به عنوان یک قبیله نوسنگی، انتقال به دوران باستان به عنوان بابل، مایاهای دوران کلاسیک، امویان قرون وسطی، بریتانیایی های دوران مدرن اولیه و غیره شوند. هر فرهنگی لایه گیم پلی مخصوص خود را اضافه می کند که منجر به نتایج تقریباً بی پایان می شود.',
