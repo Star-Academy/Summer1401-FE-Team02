7f833317ac9e929598a1f6284d1a08eb0f4050b5
Negarnbkhti42 <norob35@gmail.com>
2022-08-01 10:34:34 +0430

    [UTILS][ADD] info fetch api

--- src/app/utils/api.utils.ts
+++ src/app/utils/api.utils.ts
@@ -1,5 +1,6 @@
 export const BASE_URL = 'https://api.bijanprogrammer.com/games';
 
+export const USER_ONE = BASE_URL + '/user/one';
 export const USER_LOGIN = BASE_URL + '/user/login';
 export const USER_SIGNUP = BASE_URL + '/user/register';
 export const USER_AUTHENTICATE = BASE_URL + '/user/auth';
