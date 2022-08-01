a0e477a336c8899f7daca28eb4743bcbc60440f7
Negarnbkhti42 <norob35@gmail.com>
2022-08-01 10:34:54 +0430

    [SERVICE] [ADD] cached data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {User} from '../interfaces/User.interface';
 import {ApiService} from './api.service';
-import {USER_AUTHENTICATE, USER_LOGIN, USER_SIGNUP} from '../utils/api.utils';
+import {USER_AUTHENTICATE, USER_LOGIN, USER_ONE, USER_SIGNUP} from '../utils/api.utils';
 import {TokenObject} from '../interfaces/TokenObject.interface';
 import {IdObject} from '../interfaces/id-object';
 
@@ -9,13 +9,21 @@ import {IdObject} from '../interfaces/id-object';
     providedIn: 'root',
 })
 export class AuthService {
+    private cachedIsLoggedIn: boolean | null = null;
+    private cachedUserId: number | null = null;
+    private cachedUser: User | null = null;
+
     public constructor(private apiService: ApiService) {}
+
+    public get token(): string {
+        return localStorage.getItem('token') || '';
+    }
 
     public async login(user: Partial<User>): Promise<boolean> {
         const data = await this.apiService.post<TokenObject>(USER_LOGIN, user, {}, true);
 
         if (data?.token) {
-            localStorage.setItem('token', data?.token);
+            this.saveCache(data?.token, true, data?.id);
         }
 
         return !!data;
@@ -25,20 +33,40 @@ export class AuthService {
         const data = await this.apiService.post<TokenObject>(USER_SIGNUP, user, {}, true);
 
         if (data?.token) {
-            localStorage.setItem('token', data?.token);
+            this.saveCache(data?.token, true, data?.id);
         }
 
         return !!data;
     }
 
-    public async isLoggedIn(): Promise<boolean> {
+    public async auth(): Promise<boolean> {
         const token = localStorage.getItem('token') || '';
 
         const data = await this.apiService.post<IdObject>(USER_AUTHENTICATE, {token});
         return !!data;
     }
 
+    public async isLoggedIn(): Promise<boolean> {
+        if (this.cachedIsLoggedIn !== null) return this.cachedIsLoggedIn;
+        return await this.auth();
+    }
+
+    public async fetchUserInfo(): Promise<User | null> {
+        const response = await this.apiService.get<{user: User}>(`${USER_ONE}/${this.cachedUserId}`);
+        return response?.user || null;
+    }
+
     public logout(): void {
-        localStorage.removeItem('token');
+        this.saveCache(null, false, null);
+    }
+
+    private async saveCache(token: string | null, isLoggedIn: boolean, userId: number | null): Promise<void> {
+        if (!!token) localStorage.setItem('token', token);
+        else localStorage.removeItem('token');
+
+        this.cachedIsLoggedIn = isLoggedIn;
+        this.cachedUserId = userId;
+
+        if (this.cachedUserId) this.cachedUser = await this.fetchUserInfo();
     }
 }
