ce0d44e514ad1294718b3528ab6b1f7111602e6d
Negarnbkhti42 <norob35@gmail.com>
2022-08-01 16:39:32 +0430

    [TEST] [ADD] toast service test

--- src/app/services/toast.service.spec.ts
+++ src/app/services/toast.service.spec.ts
@@ -1,16 +1,24 @@
 import {TestBed} from '@angular/core/testing';
-
+import {ToastComponent} from '../components/toast/toast.component';
 import {ToastService} from './toast.service';
 
-describe('ToastService', () => {
+describe('SnackbarService', () => {
     let service: ToastService;
+    let toastComponentSpy: jasmine.SpyObj<ToastComponent>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(ToastService);
+        toastComponentSpy = jasmine.createSpyObj<ToastComponent>('ToastComponent', ['show']);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('tests show', () => {
+        service.setComponent(toastComponentSpy);
+        service.show('this is a message');
+        expect(toastComponentSpy.show.calls.count()).toBe(1);
     });
 });
