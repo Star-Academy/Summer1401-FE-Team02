aa416a3821de15291be05c3986ba698c3f263a19
Negarnbkhti42 <norob35@gmail.com>
2022-08-01 16:31:52 +0430

    [TEST] [UPDATE] login form

--- src/app/pages/auth/components/login-form/login-form.component.spec.ts
+++ src/app/pages/auth/components/login-form/login-form.component.spec.ts
@@ -28,6 +28,14 @@ describe('LoginFormComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should render form', () => {
+        let usernameField = host.querySelector('[name="username"]') as HTMLInputElement;
+        let passwordField = host.querySelector('[name="password"]') as HTMLInputElement;
+
+        expect(usernameField).toBeTruthy();
+        expect(passwordField).toBeTruthy();
+    });
+
     it('should change input', () => {
         let username = 'user1';
         let password = 'pass1';
@@ -47,23 +55,4 @@ describe('LoginFormComponent', () => {
             expect(passwordField.value).toEqual(password);
         });
     });
-
-    // it('should change user', fakeAsync(() => {
-    //     fixture.detectChanges();
-
-    //     let usernameField = fixture.debugElement.query(By.css('name=["username"]')).nativeElement;
-    //     // let passwordField = host.querySelector('[name="password"]') as HTMLInputElement;
-
-    //     let username = 'user2';
-    //     // let password = 'pass2';
-
-    //     usernameField.value = username;
-    //     fixture.detectChanges();
-    //     usernameField.dispatchEvent(new Event('input'));
-
-    //     fixture.detectChanges();
-    //     tick();
-
-    //     expect(component.user.username).toEqual(username);
-    // }));
 });
