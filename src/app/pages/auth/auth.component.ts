758ee67be694939c13ab6c311121bfc6f3085336
msrazavi <razavi.mst@gmail.com>
2022-08-02 16:17:50 +0430

    remove redundant import

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {User} from 'src/app/interfaces/User.interface';
-import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -11,7 +9,7 @@ import {AuthService} from 'src/app/services/auth.service';
 export class AuthComponent implements OnInit {
     public isLogin: boolean | undefined = true;
 
-    public constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) {}
+    public constructor(private router: Router, private route: ActivatedRoute) {}
 
     public ngOnInit(): void {
         this.isLogin = this.route.routeConfig?.path?.startsWith('login');
