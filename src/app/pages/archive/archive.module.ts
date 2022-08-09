d49f9168ce3d69100efd10707d3b9ac021dc14cf
Negarnbkhti42 <norob35@gmail.com>
2022-08-09 13:00:02 +0430

    setup filters

--- src/app/pages/archive/archive.module.ts
+++ src/app/pages/archive/archive.module.ts
@@ -5,9 +5,12 @@ import {ExpansionListComponent} from './components/expansion-list/expansion-list
 import {FormsModule} from '@angular/forms';
 import {SearchBoxModule} from 'src/app/components/search-box/search-box.module';
 import {FilterPipeModule} from 'src/app/pipes/filter-pipe.module';
+import {HeaderModule} from 'src/app/components/header/header.module';
+import {FooterModule} from 'src/app/components/footer/footer.module';
+import {ArchiveComponent} from './archive.component';
 
 @NgModule({
-    declarations: [FiltersComponent, ExpansionListComponent],
-    imports: [CommonModule, FormsModule, SearchBoxModule, FilterPipeModule],
+    declarations: [ArchiveComponent, FiltersComponent, ExpansionListComponent],
+    imports: [CommonModule, FormsModule, SearchBoxModule, FilterPipeModule, HeaderModule, FooterModule],
 })
 export class ArchiveModule {}
