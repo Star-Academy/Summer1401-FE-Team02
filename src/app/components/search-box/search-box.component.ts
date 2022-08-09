379f717db3f9a66e22693eec8dcb6bee6a7acad1
Negarnbkhti42 <norob35@gmail.com>
2022-08-09 12:10:42 +0430

    [ADD] expansion list

--- src/app/components/search-box/search-box.component.ts
+++ src/app/components/search-box/search-box.component.ts
@@ -7,5 +7,5 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class SearchBoxComponent {
     @Input() public value: string = '';
-    @Output() public valueChange = new EventEmitter<String>();
+    @Output() public valueChange = new EventEmitter<string>();
 }
--- src/app/pages/archive/archive.module.ts
+++ src/app/pages/archive/archive.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { ExpansionListComponent } from './components/expansion-list/expansion-list.component';
+import {FiltersComponent} from './components/filters/filters.component';
+import {ExpansionListComponent} from './components/expansion-list/expansion-list.component';
+import {FormsModule} from '@angular/forms';
+import {SearchBoxModule} from 'src/app/components/search-box/search-box.module';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-    declarations: [
-    ExpansionListComponent
-  ],
-    imports: [CommonModule],
+    declarations: [FiltersComponent, ExpansionListComponent, FilterPipe],
+    imports: [CommonModule, FormsModule, SearchBoxModule],
 })
 export class ArchiveModule {}
